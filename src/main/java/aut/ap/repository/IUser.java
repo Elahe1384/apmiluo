69d45abbc9d670ac7f91d19edb46ad167d9c365b
elahee <elahe.nejatollahi@gmail.com>
2025-06-28 14:47:07 +0330

    repository/iuser

--- src/main/java/aut/ap/repository/IUser.java
+++ src/main/java/aut/ap/repository/IUser.java
@@ -1,4 +1,16 @@
 package aut.ap.repository;
 
+import aut.ap.entity.User;
+import java.util.List;
+
 public interface IUser {
+    void addUser(User user) throws Exception;
+
+    void updateUser(User user) throws Exception;
+
+    void deleteUser(User user) throws Exception;
+
+    User findUser(String email) throws Exception;
+
+    List<User> findAllContact(User user) throws Exception;
 }
